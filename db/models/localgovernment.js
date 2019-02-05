38cd5c8060761618c6535bb4af59a8decae8ca11
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2019-02-05 12:49:57 +0100

    Add base localgovernment model and migration

---
+++ db/models/localgovernment.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const LocalGovernment = sequelize.define('LocalGovernment', {
+    name: DataTypes.STRING,
+    stateID: DataTypes.NUMBER
+  }, {});
+  LocalGovernment.associate = function(models) {
+    // associations can be defined here
+    models.LocalGovernment.belongsTo(models.State, {
+      onDelete: 'CASCADE',
+      foreignKey: {
+        name: 'stateID',
+        allowNull: false
+      }
+    })
+
+    models.LocalGovernment.hasMany(models.Business, { as: 'localGovernmentID' })
+  };
+  return LocalGovernment;
+};
